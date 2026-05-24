77ef7836e8d58bdf2ed04c0d1471983a35ce980b
idotsenko <idotsenko@usergate.com>
2026-05-24 12:36:08 +0300

    awd

--- _opinions_app.py
+++ _opinions_app.py
@@ -1,10 +1,8 @@
 # what_to_watch/opinions_app.py
 
-import csv
 from datetime import datetime
 from random import randrange
 
-import click
 from flask import Flask, abort, flash, redirect, render_template, url_for
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -94,18 +92,7 @@ def internal_error(error):
     return render_template('500.html'), 500
 
 
-@app.cli.command('load_opinions')
-def load_opinions_command():
-    """Функция загрузки мнений в базу данных."""
-    with open('opinions.csv', encoding='utf-8') as f:
-        reader = csv.DictReader(f)
-        counter = 0
-        for row in reader:
-            opinion = Opinion(**row)
-            db.session.add(opinion)
-            db.session.commit()
-            counter += 1
-    click.echo(f'Загружено мнений: {counter}')
+
 
 
 if __name__ == '__main__':
