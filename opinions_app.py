2a567fc3742fda03539238b8014bf958b52fa96f
idotsenko <idotsenko@usergate.com>
2026-05-24 06:43:04 +0300

    awd

--- opinions_app.py
+++ opinions_app.py
@@ -26,16 +26,23 @@ class Opinion(db.Model):
 class OpinionForm(FlaskForm):
     title = StringField(
         'Введите название фильма',
-        validators=[DataRequired(message='Обязательное поле'),
-                    Length(1, 128)]
+        validators=[
+            DataRequired(message='Обязательное поле'),
+            Length(1, 128)
+        ]
     )
     text = TextAreaField(
         'Напишите мнение',
-        validators=[DataRequired(message='Обязательное поле')]
+        validators=[
+            DataRequired(message='Обязательное поле')
+        ]
     )
     source = URLField(
         'Добавьте ссылку на подробный обзор фильма',
-        validators=[Length(1, 256), Optional()]
+        validators=[
+            Length(1, 256),
+            Optional()
+        ]
     )
     submit = SubmitField('Добавить')
 
