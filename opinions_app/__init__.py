a82512adfa8a6281147e1a1ab15a592f5f33635d
idotsenko <idotsenko@usergate.com>
2026-05-24 12:53:05 +0300

    api_views

--- opinions_app/__init__.py
+++ opinions_app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from settings import Config
+from .settings import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from . import cli_commands, error_handlers, views
+from . import cli_commands, error_handlers, views, api_views
