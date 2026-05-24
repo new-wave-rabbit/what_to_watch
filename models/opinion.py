cbf8b04f5f2f7657246c201eb8242ebcd5e67b3a
idotsenko <idotsenko@usergate.com>
2026-05-24 06:40:38 +0300

    awd

---
+++ models/opinion.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+from opinions_app import app
+
+db = SQLAlchemy(app)
+
+
+class Opinion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    text = db.Column(db.Text, unique=True, nullable=False)
+    source = db.Column(db.String(256))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
