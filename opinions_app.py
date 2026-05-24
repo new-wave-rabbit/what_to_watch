ee3ba928215ca916319cb1c7e082d1ce316d85f0
idotsenko <idotsenko@usergate.com>
2026-05-24 12:27:47 +0300

    awd

--- opinions_app.py
+++ opinions_app.py
@@ -1,21 +1,24 @@
+# what_to_watch/opinions_app.py
+
+import csv
 from datetime import datetime
 from random import randrange
 
+import click
 from flask import Flask, abort, flash, redirect, render_template, url_for
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, URLField
 from wtforms.validators import DataRequired, Length, Optional
 
-
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-app.config['SECRET_KEY'] = 'MY SECRET KEY'
+app.config['SECRET_KEY'] = 'SECRET KEY'
 
 db = SQLAlchemy(app)
+
 migrate = Migrate(app, db)
 
 
@@ -25,20 +28,22 @@ class Opinion(db.Model):
     text = db.Column(db.Text, unique=True, nullable=False)
     source = db.Column(db.String(256))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    added_by = db.Column(db.String(64))
 
 
 class OpinionForm(FlaskForm):
     title = StringField(
         'Введите название фильма',
-        validators=[DataRequired(message='Обязательное поле'), Length(1, 128)],
+        validators=[DataRequired(message='Обязательное поле'),
+                    Length(1, 128)]
     )
     text = TextAreaField(
-        'Напишите мнение',
-        validators=[DataRequired(message='Обязательное поле')],
+        'Напишите мнение', 
+        validators=[DataRequired(message='Обязательное поле')]
     )
     source = URLField(
         'Добавьте ссылку на подробный обзор фильма',
-        validators=[Length(1, 256), Optional()],
+        validators=[Length(1, 256), Optional()]
     )
     submit = SubmitField('Добавить')
 
@@ -47,8 +52,6 @@ class OpinionForm(FlaskForm):
 def index_view():
     quantity = Opinion.query.count()
     if not quantity:
-        # Если в базе пусто - при запросе к главной странице
-        # пользователь увидит ошибку 500.
         abort(500)
     offset_value = randrange(quantity)
     opinion = Opinion.query.offset(offset_value).first()
@@ -60,15 +63,12 @@ def add_opinion_view():
     form = OpinionForm()
     if form.validate_on_submit():
         text = form.text.data
-        # Если в БД уже есть мнение с текстом, который ввёл пользователь...
         if Opinion.query.filter_by(text=text).first() is not None:
-            # ...вызвать функцию flash и передать соответствующее сообщение.
             flash('Такое мнение уже было оставлено ранее!')
-            # Вернуть пользователя на страницу «Добавить новое мнение».
             return render_template('add_opinion.html', form=form)
         opinion = Opinion(
-            title=form.title.data,
-            text=text,
+            title=form.title.data, 
+            text=text, 
             source=form.source.data
         )
         db.session.add(opinion)
@@ -77,10 +77,15 @@ def add_opinion_view():
     return render_template('add_opinion.html', form=form)
 
 
-@app.route('/opinions/<int:id>')
+@app.route('/opinion/<int:id>')
 def opinion_view(id):
     opinion = Opinion.query.get_or_404(id)
     return render_template('opinion.html', opinion=opinion)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
 
 
 @app.errorhandler(500)
@@ -89,9 +94,18 @@ def internal_error(error):
     return render_template('500.html'), 500
 
 
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template('404.html'), 404
+@app.cli.command('load_opinions')
+def load_opinions_command():
+    """Функция загрузки мнений в базу данных."""
+    with open('opinions.csv', encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+        counter = 0
+        for row in reader:
+            opinion = Opinion(**row)
+            db.session.add(opinion)
+            db.session.commit()
+            counter += 1
+    click.echo(f'Загружено мнений: {counter}')
 
 
 if __name__ == '__main__':
