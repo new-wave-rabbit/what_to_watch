fb21057c15b6f8738f55d4439e8f0985a45c0a49
idotsenko <idotsenko@usergate.com>
2026-05-26 19:49:15 +0300

    awd

--- opinions_app/api_views.py
+++ opinions_app/api_views.py
@@ -2,25 +2,22 @@ from flask import jsonify, request
 
 from . import app, db
 from .models import Opinion
-
+from .views import random_opinion
 
 @app.route('/api/opinions/<int:id>/', methods=['GET'])
 def get_opinion(id):
     opinion = Opinion.query.get_or_404(id)
     return jsonify({'opinion': opinion.to_dict()}), 200
 
-
 @app.route('/api/opinions/<int:id>/', methods=['PATCH'])
 def update_opinion(id):
-    data = request.get_json()
     opinion = Opinion.query.get_or_404(id)
-    opinion.title = data.get('title', opinion.title)
-    opinion.text = data.get('text ', opinion.text)
-    opinion.source = data.get('source', opinion.source)
-    opinion.added_by = data.get('added_by', opinion.added_by)
-    db.session.commit()
-    return jsonify({'opinion': opinion.to_dict()}), 200
-
+    opinion.title = request.json.get('title', opinion.title)
+    opinion.text = request.json.get('text', opinion.text)
+    opinion.source = request.json.get('source', opinion.source)
+    opinion.added_by = request.json.get('added_by', opinion.added_by)
+    db.session.commit()  
+    return jsonify({'opinion': opinion.to_dict()}), 201
 
 @app.route('/api/opinions/<int:id>/', methods=['DELETE'])
 def delete_opinion(id):
@@ -29,19 +26,22 @@ def delete_opinion(id):
     db.session.commit()
     return '', 204
 
-
 @app.route('/api/opinions/', methods=['GET'])
 def get_opinions():
-    opinions = Opinion.query.all()
+    opinions = Opinion.query.all()  
     opinions_list = [opinion.to_dict() for opinion in opinions]
-    return jsonify(opinions_list), 200
-
+    return jsonify({'opinions': opinions_list}), 200
 
 @app.route('/api/opinions/', methods=['POST'])
 def add_opinion():
-    data = request.get_json()
-    opinion = Opinion()
-    opinion.from_dict(data=data)
-    db.session.add(opinion)
-    db.session.commit()
+    data = request.get_json()  
+    opinion = Opinion()  
+    opinion.from_dict(data)  
+    db.session.add(opinion)  
+    db.session.commit()  
+    return jsonify({'opinion': opinion.to_dict()}), 201
+
+@app.route('/api/get-random-opinion/', methods=['GET'])
+def get_random_opinion():
+    opinion = random_opinion()
     return jsonify({'opinion': opinion.to_dict()}), 200
