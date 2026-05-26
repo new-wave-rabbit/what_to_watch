cf2727cc2c54ce99ba84b19f52273b7d8e3c0515
idotsenko <idotsenko@usergate.com>
2026-05-26 19:14:14 +0300

    add_opinion

--- opinions_app/api_views.py
+++ opinions_app/api_views.py
@@ -21,10 +21,27 @@ def update_opinion(id):
     db.session.commit()
     return jsonify({'opinion': opinion.to_dict()}), 200
 
+
 @app.route('/api/opinions/<int:id>/', methods=['DELETE'])
 def delete_opinion(id):
     opinion = Opinion.query.get_or_404(id)
     db.session.delete(opinion)
     db.session.commit()
     return '', 204
-    
+
+
+@app.route('/api/opinions/', methods=['GET'])
+def get_opinions():
+    opinions = Opinion.query.all()
+    opinions_list = [opinion.to_dict() for opinion in opinions]
+    return jsonify(opinions_list), 200
+
+
+@app.route('/api/opinions/', methods=['POST'])
+def add_opinion():
+    data = request.get_json()
+    opinion = Opinion()
+    opinion.from_dict(data=data)
+    db.session.add(opinion)
+    db.session.commit()
+    return jsonify({'opinion': opinion.to_dict()}), 200
