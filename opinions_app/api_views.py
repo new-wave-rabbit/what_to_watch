0dba99df598d0ccba7ba28e275ed9488af64f2ba
idotsenko <idotsenko@usergate.com>
2026-05-26 18:39:02 +0300

    awd

--- opinions_app/api_views.py
+++ opinions_app/api_views.py
@@ -1,23 +1,10 @@
-# what_to_watch/opinions_app/api_views.py
-
 from flask import jsonify
 
 from . import app
 from .models import Opinion
 
-def opinion_to_dict(opinion):
-    return dict(
-        id = opinion.id,
-        title = opinion.title,
-        text = opinion.text,
-        source = opinion.source,
-        timestamp = opinion.timestamp,
-        added_by = opinion.added_by
-    )
 
 @app.route('/api/opinions/<int:id>/', methods=['GET'])
 def get_opinion(id):
     opinion = Opinion.query.get_or_404(id)
-    # Конвертировать объект модели в словарь.
-    data = opinion_to_dict(opinion)  
-    return jsonify({'opinion': data}), 200
+    return jsonify({'opinion': opinion.to_dict()}), 200
--- opinions_app/settings.py
+++ opinions_app/settings.py
@@ -4,3 +4,5 @@ import os
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SECRET_KEY = os.getenv('SECRET_KEY')
+    JSON_AS_ASCII = False
+    JSON_PROVIDER_CLASS = None
